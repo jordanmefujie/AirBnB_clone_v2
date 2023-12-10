634e0cf1a0c66a062e242c0afd5908c86617ef98
jordanmefujie <mjordanlonpo@gmail.com>
2023-12-10 14:11:59 -0800

    Preparing my web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
+# script that sets up web servers for the deployment of web_static
 # Install Nginx if not already installed
 if ! command -v nginx &> /dev/null; then
-            sudo apt-get update
-                sudo apt-get -y install nginx
+    sudo apt-get update
+    sudo apt-get -y install nginx
 fi
 
 # Create necessary folders
@@ -10,7 +11,13 @@ sudo mkdir -p /data/web_static/{releases/test,shared}
 sudo chown -R ubuntu:ubuntu /data/
 
 # Create a fake HTML file
-echo "<html><head></head><body>Hello Holberton!</body></html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Hello Holberton!
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html > /dev/null
 
 # Create or recreate symbolic link
 sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
@@ -22,21 +29,19 @@ server {
     server_name _;
 
     location /hbnb_static {
-
-         alias /data/web_static/current/;
-
-         index index.html;
+        alias /data/web_static/current/;
+        index index.html;
     }
 
-   location /redirect_me {
+    location /redirect_me {
         return 301 http://www.google.com;
-   }
+    }
 
-   error_page 404 /404.html;
-   location /404 {
+    error_page 404 /404.html;
+    location /404 {
         alias /custom_404.html;
         internal;
-  }
+        }
 }"
 echo "$config_content" | sudo tee /etc/nginx/sites-available/default > /dev/null
 
